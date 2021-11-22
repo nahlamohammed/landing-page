92b29074a5cb375fc7333cf8395eab0bf27366e2
nahlazaki <eng.nahla.mohammed@gmail.com>
2021-11-22 15:28:50 +0200

    dynamic landing page

--- README.md
+++ README.md
@@ -1,13 +1,18 @@
 # Landing Page Project
+This project requires you to build a multi-section landing page, with a dynamically updating navigational menu based on the amount of content that is added to the page.
 
 ## Table of Contents
 
-* [Instructions](#instructions)
+* [Dependencies](#dependencies)
+* 
+* [JS Version](#JSVersion)
 
-## Instructions
+## Dependencies
+None
 
-The starter project has some HTML and CSS styling to display a static version of the Landing Page project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
+## JSVersion
+ES2015/ES6
 
-To get started, open `js/app.js` and start building out the app's functionality
 
-For specific, detailed instructions, look at the project instructions in the Udacity Classroom.
+
+
--- js/app.js
+++ js/app.js
@@ -13,50 +13,149 @@
  * 
 */
 
-/**
- * Comments should be present at the beginning of each procedure and class.
- * Great to have comments before crucial code sections within the procedure.
-*/
+const main = () =>{
+    buildSections(4);
+    buildNavigationMenu();
+}
+
+//number of sections in page
+let sectionsNumber = 0;
 
 /**
- * Define Global Variables
- * 
-*/
+ * [build a section]
+ *
+ */
+const buildSection = () => {
+    sectionsNumber ++;
+    //create section element and set its attributes
+    const section = document.createElement('section');
+    section.setAttribute('id', `section${sectionsNumber}`);
+    section.setAttribute('data-nav', `Section ${sectionsNumber}`);
+    section.innerHTML = `<div class="landing__container">
+    <h2>Section ${sectionsNumber}</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum metus faucibus lectus pharetra dapibus. Suspendisse potenti. Aenean aliquam elementum mi, ac euismod augue. Donec eget lacinia ex. Phasellus imperdiet porta orci eget mollis. Sed convallis sollicitudin mauris ac tincidunt. Donec bibendum, nulla eget bibendum consectetur, sem nisi aliquam leo, ut pulvinar quam nunc eu augue. Pellentesque maximus imperdiet elit a pharetra. Duis lectus mi, aliquam in mi quis, aliquam porttitor lacus. Morbi a tincidunt felis. Sed leo nunc, pharetra et elementum non, faucibus vitae elit. Integer nec libero venenatis libero ultricies molestie semper in tellus. Sed congue et odio sed euismod.</p>
 
-
-/**
- * End Global Variables
- * Start Helper Functions
- * 
-*/
-
-
-
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
-
-// build the nav
-
-
-// Add class 'active' to section when near top of viewport
-
-
-// Scroll to anchor ID using scrollTO event
-
+    <p>Aliquam a convallis justo. Vivamus venenatis, erat eget pulvinar gravida, ipsum lacus aliquet velit, vel luctus diam ipsum a diam. Cras eu tincidunt arcu, vitae rhoncus purus. Vestibulum fermentum consectetur porttitor. Suspendisse imperdiet porttitor tortor, eget elementum tortor mollis non.</p>
+    </div>`;
+    //append section to main element
+    document.querySelector('main').appendChild(section);
+}
 
 /**
- * End Main Functions
- * Begin Events
- * 
-*/
+ * [build number of sections dynamically]
+ *
+ * @param   {[type]}  num  [num of section to be created]
+ *
+ */
+const buildSections = (num) => {
+    for(let i = 0; i < num; i++){
+        const section = buildSection(sectionsNumber);        
+    }
+}
 
-// Build menu 
+/**
+ * [build Navigation Menu]
+ *
+ */
+const buildNavigationMenu = () => {
+    const navbar = document.querySelector('#navbar__list');
+    navbar.innerHTML = '';
+    //create fregment to wrap the lis
+    const fragment = document.createDocumentFragment();
+    //get all sections
+    const sections = [...document.querySelectorAll('main section')];
+    //loop for all sections to create li according to its data-nav
+    sections.forEach(section => {
+        const listItem = document.createElement('li');
+        //create anchor, set its class and content and add event listner to it
+        const anchor = document.createElement('a');
+        anchor.setAttribute('href', `#${section.getAttribute('id')}`);
+        anchor.className = 'menu__link';
+        anchor.textContent = section.getAttribute('data-nav');
+        anchor.addEventListener('click', clickAnchorLink);
+        //append children
+        listItem.appendChild(anchor);
+        fragment.appendChild(listItem);
+    });
+    navbar.appendChild(fragment);
+}
 
-// Scroll to section on link click
+/**
+ * [toggle Active Classes]
+ *
+ */
+const toggleActiveClasses = () => {
+    const sections = [...document.querySelectorAll('main section')];
+    //loop for all sections to create li according to its data-nav
+    sections.forEach(section => {
+        const anchor = document.querySelector(`a[href='#${section.getAttribute('id')}']`);
+        const sectionTop = section.getBoundingClientRect().top;
+        if(sectionTop >=0 && sectionTop <= 400){
+            section.classList.add('active');
+            anchor.classList.add('active__link');
+        }else{
+            section.classList.remove('active');
+            anchor.classList.remove('active__link');
+        }
+    });
+}
 
-// Set sections as active
+/**
+ * [clickAnchorLink description]
+ *
+ * @param   {[type]}  event  [event]
+ *
+ */
+const clickAnchorLink = (event) => {
+    const anchors = [...document.querySelectorAll('li a')];
+    anchors.forEach(anchor => {
+        if(event.target === anchor){
+            anchor.classList.add('active__link');
+            const sectionId = anchor.getAttribute('href').slice(1);
+            document.querySelector(`section[id='${sectionId}']`).scrollIntoView({
+                behavior: 'smooth', block: 'start'
+            });
+        }else{
+            anchor.classList.remove('active__link');
+        }
+    });
+    event.preventDefault();
+}
+
+let isScrolling;
+const header = document.querySelector('header');
+/**
+ * [hide or display the header]
+ *
+ */
+const toggleHeader = () => {
+    header.style.display = 'block';
+	// Clear timeout throughout the scroll
+	window.clearTimeout( isScrolling );
+	// Set a timeout to run after scrolling ends
+	isScrolling = setTimeout(()=> {
+		// Run the callback
+		header.style.display = 'none';
+	}, 3000);
+}
+
+//add events when scrolling the window
+window.addEventListener('scroll', () => {
+    toggleActiveClasses();
+    toggleHeader();
+});
+
+//add events when click to add new section
+document.querySelector('#addSection').addEventListener('click', () =>{
+    buildSection();
+    buildNavigationMenu();
+});
+
+//Run at the start
+main();
+
+
+
+
 
 
